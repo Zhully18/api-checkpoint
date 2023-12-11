0ed1c89c245347c73a877f546b806878b4b6294c
Zhully <agunregezhuliakho@gmail.com>
2023-12-11 06:03:54 -0800

    api checkpoint

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+import UserList from './UserList';
+
+const App = () => {
+  return (
+    <div className="app">
+      <UserList />
+    </div>
+  );
+};
+
+export default App;
